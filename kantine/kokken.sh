ebb443ebd71103da213872fe294797c60b24d536
joneikholm <2873703+joneikholm@users.noreply.github.com>
2026-09-24 23:00:02 +0200

    hjemmeopg 6

--- kantine/kokken.sh
+++ kantine/kokken.sh
@@ -3,3 +3,7 @@ docker run -d --name myNginx nginx
 docker exec myNginx ls /etc/nginx/conf.d
 docker logs myNginx
 docker run -d --name myNginx2 nginx
+docker stop myNginx
+docker stop myNginx2
+docker rm myNginx
+docker rm myNginx2

3d35fbdb3ebfe1fac6c16894cda32872a6020af9
joneikholm <2873703+joneikholm@users.noreply.github.com>
2026-09-24 21:31:28 +0200

    hjemmeopg 1

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,1 @@
-FROM python:3.12-alpine
-WORKDIR /app
-COPY index.html .
-CMD ["python", "-m", "http.server", "8000"]
+FROM alpine
--- image.sh
+++ image.sh
@@ -1,3 +1,4 @@
+set -e
 docker build -t minside .
 docker run -d --name mincontainer -p 9090:8000 minside
 sleep 2 && curl -f localhost:9090
